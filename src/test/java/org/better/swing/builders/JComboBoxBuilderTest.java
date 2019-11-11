743832bfdf88aebff445db024c8eb370f09f790f
DanVanAtta <dhvatta@gmail.com>
2019-11-11 10:43:08 -0800

    Update build for maven upload

--- src/test/java/org/better/swing/builders/JComboBoxBuilderTest.java
+++ src/test/java/org/better/swing/builders/JComboBoxBuilderTest.java
@@ -2,29 +2,24 @@ package org.better.swing.builders;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import com.example.mockito.MockitoExtension;
 import java.awt.event.ItemEvent;
 import java.util.concurrent.atomic.AtomicInteger;
-
 import javax.swing.JComboBox;
-
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class JComboBoxBuilderTest {
-  @Mock
-  private ItemEvent mockItemEvent;
+  @Mock private ItemEvent mockItemEvent;
 
   @Test
   public void buildInvalidEmptyOptionSet() {
-    assertThrows(IllegalArgumentException.class, () -> JComboBoxBuilder.builder()
-        .menuOptions()
-        .build());
+    assertThrows(
+        IllegalArgumentException.class, () -> JComboBoxBuilder.builder().menuOptions().build());
   }
 
   @Test
@@ -34,9 +29,8 @@ public class JComboBoxBuilderTest {
 
   @Test
   public void basicBuilderWithMenuOptions() {
-    final JComboBox<String> box = JComboBoxBuilder.builder()
-        .menuOptions("option 1", "option 2", "option 3")
-        .build();
+    final JComboBox<String> box =
+        JComboBoxBuilder.builder().menuOptions("option 1", "option 2", "option 3").build();
 
     MatcherAssert.assertThat(box.getSelectedIndex(), Is.is(0));
     MatcherAssert.assertThat(box.getItemCount(), Is.is(3));
@@ -52,15 +46,17 @@ public class JComboBoxBuilderTest {
     final AtomicInteger triggerCount = new AtomicInteger(0);
 
     final String secondOption = "option 2";
-    final JComboBox<String> box = JComboBoxBuilder.builder()
-        .menuOptions("option 1", secondOption, "option 3")
-        .itemListener(value -> {
-          if (value.equals(secondOption)) {
-            triggerCount.incrementAndGet();
-          }
-        }).build();
+    final JComboBox<String> box =
+        JComboBoxBuilder.builder()
+            .menuOptions("option 1", secondOption, "option 3")
+            .itemListener(
+                value -> {
+                  if (value.equals(secondOption)) {
+                    triggerCount.incrementAndGet();
+                  }
+                })
+            .build();
     box.setSelectedIndex(1);
     MatcherAssert.assertThat(triggerCount.get(), Is.is(1));
   }
-
 }
